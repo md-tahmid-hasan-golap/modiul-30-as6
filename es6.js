341472796cc4c5fc40cfedc3f509a4a041c9f1e7
golap-raj <golapraj47@gmail.com>
2025-03-05 02:03:25 +0600

    first commit

---
+++ es6.js
@@ -0,0 +1,26 @@
+const countryName = "Bangladesg";
+console.log(countryName);
+// man change hobe na ata constant diye diclar kora ase
+
+// countryName = "bd";
+// console.log(countryName)
+
+let age = 20;
+console.log(age);
+
+age = 21;
+console.log(age)
+
+
+function sum (num1, num2){
+    const result = num1 + num2;
+    console.log(result)
+}
+sum(10);
+
+function gun (num1=1, num2=1){
+    const result = num1 * num2;
+    console.log(result)
+}
+
+gun(10)
